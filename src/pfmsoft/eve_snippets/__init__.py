043102faa5d6b150d89849e474c64c04d0dce614
Chad Lowe <pfmsoft.dev@gmail.com>
2026-07-18 05:28:12 -0700

    add workflow for publishing to PyPI and release script
    * Create GitHub Actions workflow for automated PyPI publishing
    * Add release script to manage version bumps and git operations
    * Update app name and version retrieval in __init__.py

--- src/pfmsoft/eve_snippets/__init__.py
+++ src/pfmsoft/eve_snippets/__init__.py
@@ -1,13 +1,15 @@
+from importlib.metadata import version
+
 __project_namespace__ = "pfmsoft"
 __author__ = "Chad Lowe"
 __email__ = "pfmsoft.dev@gmail.com"
-__app_name__ = "eve-snippets"
+__app_name__ = "pfmsoft-eve-snippets"  # Must match project.name from pyproject.toml
 #######################################################################################
 # Update in pyproject.toml, as uv build backend does not yet support dynamic metadata #
 # https://github.com/astral-sh/uv/issues/11718                                        #
 #######################################################################################
 __description__ = "A command line first interface to the Eve Online API"
-__version__ = "0.1.0"
+__version__ = version(__app_name__)
 __release__ = __version__
 #######################################################################################
 __url__ = "https://github.com/DonalChilde/pfmsoft-eve-snippets"
