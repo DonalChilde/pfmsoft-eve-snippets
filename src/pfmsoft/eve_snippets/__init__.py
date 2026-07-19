061760b8c0ea352bdb2b50413a1caf85ffc638e1
Chad Lowe <pfmsoft.dev@gmail.com>
2026-07-19 10:05:20 -0700

    update project metadata in pyproject.toml and __init__.py
    * Refine project description for clarity
    * Adjust authors formatting for consistency
    * Remove unnecessary comments in __init__.py

--- src/pfmsoft/eve_snippets/__init__.py
+++ src/pfmsoft/eve_snippets/__init__.py
@@ -1,17 +1,14 @@
+"""Eve Online API snippets for Python."""
+
 from importlib.metadata import version
 
 __project_namespace__ = "pfmsoft"
 __author__ = "Chad Lowe"
 __email__ = "pfmsoft.dev@gmail.com"
 __app_name__ = "pfmsoft-eve-snippets"  # Must match project.name from pyproject.toml
-#######################################################################################
-# Update in pyproject.toml, as uv build backend does not yet support dynamic metadata #
-# https://github.com/astral-sh/uv/issues/11718                                        #
-#######################################################################################
-__description__ = "A command line first interface to the Eve Online API"
+__description__ = "Python code snippets for working with the Eve Online API."
 __version__ = version(__app_name__)
 __release__ = __version__
-#######################################################################################
 __url__ = "https://github.com/DonalChilde/pfmsoft-eve-snippets"
 __license__ = "MIT"
 
